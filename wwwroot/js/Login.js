363b9c0377e8c1bd3980ab81db581cb20965295d
Wellwhale <minpccl1@gmail.com>
2026-02-16 20:18:40 +0700

    add show password icon and make it work

--- wwwroot/js/Login.js
+++ wwwroot/js/Login.js
@@ -12,4 +12,21 @@ function Login_page(){
     })
 }
 
+function Show_password(){
+    const show_pw = document.querySelector('#show-passwd');
+    const show_pw_icon = show_pw.querySelector('img');
+    const pw_input = document.querySelector('#password');
+
+    show_pw.addEventListener('click', function(){
+        pw_input.type = pw_input.type === 'password' 
+            ? 'text' 
+            : 'password'; // check type and return it
+        
+        show_pw_icon.src = pw_input.type === 'password' 
+            ? 'image/eye_open.svg' 
+            : 'image/eye_closed.svg'
+    })
+}
+
+Show_password();
 Login_page();
