405b4b36c30001a2dd34f420a577b360cf27f2c0
Wellwhale <minpccl1@gmail.com>
2026-02-21 00:19:54 +0700

    fix bug submit button -> now work together correctly -> optimal: make regeg for email input box (Why not! ^_^)

--- wwwroot/js/Register.js
+++ wwwroot/js/Register.js
@@ -1,5 +1,3 @@
-
-
 function Show_password(){
     const show_pw_2 = document.querySelectorAll('.show-passwd');
     show_pw_2.forEach(show_pw => {
@@ -25,6 +23,7 @@ function Show_password(){
 function Css_submit(valid){
     const sub_button = document.querySelector('.submit-button');
     sub_button.disabled = !valid ;
+    console.log("now button is :",valid)
     if(sub_button.disabled){
             sub_button.style.pointerEvents = "none";
 
@@ -42,66 +41,66 @@ function Css_submit(valid){
 }
 
 
+
 function Disable_submit(){
     const input_box = document.querySelectorAll('input[required]');
-
-    function check_form(){
-        let valid = true;
-
-        input_box.forEach(function(input){
-            if(!input.value.trim() || !input.checkValidity()){ // trim for check " " -> string spacebar
-                valid = false;
-            }
-        })
-        
-        Css_submit(valid);  // change css submit-button
-        
-    }
-
-    input_box.forEach(function(input){       // ใส่ function 
+    input_box.forEach(function(input){       
         input.addEventListener('input', check_form);
     })
     check_form();
 }
 
-function Compare_pwd(){
-    
-    const pwd_input = document.querySelectorAll(".pwd_input_box");
-    
-    function Check_pwd(){
-        const pwd = document.querySelector("#pwd-1").value;
-        const con_pwd = document.querySelector("#pwd-2").value;
 
-        let valid = true
-        let show_err = false;
-
-        // console.log(pwd , con_pwd);
-
-        if(pwd=="" || con_pwd==""){
+function check_form(){
+    const input_box = document.querySelectorAll('input[required]');
+    let valid = true;
+    input_box.forEach(function(input){
+        if(!input.value.trim() || !input.checkValidity()){ // trim for check " " -> string spacebar
             valid = false;
-            show_err = false;
         }
-        else if ( pwd != con_pwd ){
-            valid = false;
-            show_err = true;
-        }
-        
-        // console.log(valid, show_err);
-
-        Css_submit(valid);
-        document.querySelector("#pwd_not_match").style.display = "none";
-        if(show_err){
-            document.querySelector("#pwd_not_match").style.display = "block";
-            // console.log('Errrrrrrrrr');
-        }
-    } 
-
-    pwd_input.forEach(function(input){
-        input.addEventListener('input',Check_pwd)
     })
+    Css_submit(valid && Check_pwd());
 }
+
+function Check_pwd(){
+    const pwd = document.querySelector("#pwd-1").value;
+    const con_pwd = document.querySelector("#pwd-2").value;
+
+    let valid = true
+    let show_err = false;
+
+    // console.log(pwd , con_pwd);
+
+    if(pwd=="" || con_pwd==""){
+        valid = false;
+        show_err = false;
+    }
+    else if ( pwd != con_pwd ){
+        valid = false;
+        show_err = true;
+    }
+    
+    document.querySelector("#pwd_not_match").style.display = "none";
+    if(show_err){
+        document.querySelector("#pwd_not_match").style.display = "block";
+    }
+    return valid
+} 
+
+
+// function Compare_pwd(){
+
+//     const pwd_input = document.querySelectorAll(".pwd_input_box");
+
+//     pwd_input.forEach(function(input){
+//         input.addEventListener('input', function(){
+
+//             Css_submit(check_form() && Check_pwd());
+//         })
+//     })
+// }
 
 
 Disable_submit();   
-Compare_pwd()
+// Compare_pwd()
 Show_password();
