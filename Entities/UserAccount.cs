1a37689be79110c2d213e34821b5a39ae710ae97
SansTalesS012T <Underverse678@gmail.com>
2026-02-20 14:30:40 +0700

    change namespace method

--- Entities/UserAccount.cs
+++ Entities/UserAccount.cs
@@ -2,46 +2,44 @@ using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
-namespace KMITL_WebDev_MiniProject.Entites
+namespace KMITL_WebDev_MiniProject.Entites;
+[Index(nameof(Email), IsUnique = true)]
+[Index(nameof(UserName), IsUnique = true)]
+[Index(nameof(RealUserName), IsUnique = true)]
+public class UserAccount: IdentityUser
 {
-	[Index(nameof(Email), IsUnique = true)]
-	[Index(nameof(UserName), IsUnique = true)]
-	[Index(nameof(RealUserName), IsUnique = true)]
-	public class UserAccount: IdentityUser
-	{
-		[Key]
-		public int Id {get; set;}
+	[Key]
+	public int Id {get; set;}
 
-		[Required]
-		public string FirstName {get; set;}
+	[Required]
+	public string FirstName {get; set;}
 
-		[Required]
-		public string LastName {get; set;}
+	[Required]
+	public string LastName {get; set;}
 
-		[Required]
-		[DataType(DataType.EmailAddress)]
-		public string Email {get; set;}
+	[Required]
+	[DataType(DataType.EmailAddress)]
+	public string Email {get; set;}
 
-		[Required]
-		public override string UserName {get; set;}
+	[Required]
+	public override string UserName {get; set;}
 
-		[Required]
-		public string RealUserName {get; set;}
+	[Required]
+	public string RealUserName {get; set;}
 
-		[Required]
-		public uint Sex {get; set;}
+	[Required]
+	public uint Sex {get; set;}
 
-		[Required]
-		public int Reputation {get; set;}
+	[Required]
+	public int Reputation {get; set;}
 
-		[Required]
-		[DataType(DataType.PhoneNumber)]
-		public override string PhoneNumber {get; set;}
+	[Required]
+	[DataType(DataType.PhoneNumber)]
+	public override string PhoneNumber {get; set;}
 
-		[Required]
-		[DataType(DataType.Date)]
-		public DateOnly DateOfBirth {get; set;}
+	[Required]
+	[DataType(DataType.Date)]
+	public DateOnly DateOfBirth {get; set;}
 
-		public string? ImageURL {get; set;}
-	}
-};
+	public string? ImageURL {get; set;}
+}
